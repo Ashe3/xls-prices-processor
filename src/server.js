a1cbd516dbc6ee003d5ea4106a78a4a7b7b1bb81
Anton Shevchenko <korolewky.anton3@gmail.com>
2021-02-07 20:18:08 +0200

    improved structure

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const router = require('./routes/mainRouter');
+const routes = require('./api/routes/index');
 
 const app = express();
 const PORT = process.env.PORT || process.env.PORT_DEFAULT;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
+
+app.use('/api', routes);
 app.use('/', router);
 
 
